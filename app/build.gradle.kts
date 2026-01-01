e3dfe21c2fc469a55a7de24a732ed54e7e0a7752
applefarmm <dafi.rasyadan@gmail.com>
2026-01-01 12:09:40 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.fida.app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.fida.app"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("nl.joery.timerangepicker:timerangepicker:1.0.0")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.mikhaellopez:circularprogressbar:3.1.0")
+
+
+}
